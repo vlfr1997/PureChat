4715c981b42eb0372d1f4cdcb5a863bbe2f6aa41
vlfr1997 <49419833+vlfr1997@users.noreply.github.com>
2021-02-27 01:03:58 -0300

    Update PureChat.php

--- src/_64FF00/PureChat/PureChat.php
+++ src/_64FF00/PureChat/PureChat.php
@@ -478,7 +478,13 @@ class PureChat extends PluginBase
         $string = str_replace("{prefix}", $this->getPrefix($player, $levelName), $string);
         $string = str_replace("{suffix}", $this->getSuffix($player, $levelName), $string);
         try {
-            $string = str_replace("{rankup}", $this->purePerms->getPlayerRank($player), $string);
+            $group = $this->purePerms->getUserDataMgr()->getData($player)['group'];
+
+			if($group !== null){
+				$string = str_replace("{rankup}", $group, $string);
+			}else{
+                $string = str_replace("{rankup}", "No Rank", $string);;
+            }
         } catch (\Throwable $th) {
             echo $th;
         }
